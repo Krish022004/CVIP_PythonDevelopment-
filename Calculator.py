2cabab9a2ab24557368294d099bd9276cd8bc660
gkt02 <gkt02feb2004@gmail.com>
2023-08-31 00:26:49 +0530

    These Python Development intern phase 1 normal project

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,31 @@
 import math
+
+
 def add(x, y):
     return x + y
+
+
 def sub(x, y):
     return x-y
-def multiply(x,y):
+
+
+def multiply(x, y):
     return x*y
+
+
 def divide(x,y):
     return x/y
+
+
 def sqr(x):
     return math.sqrt(x)
+
+
 def power(x,y):
     return math.pow(x,y)
 
 
-print("Select opertion")
+print("Select operation")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
@@ -21,38 +33,38 @@ print("4.Divide")
 print("5.Square root")
 print("6.Power")
 while True:
-    choice = input("enter choice (1/2/3/4):")
+    choice = input("Enter choice (1/2/3/4):")
     if choice in ('1','2','3','4'):
         try:
-            num1=int(input("Enter first number:"))
-            num2 =int(input("Enter second number:"))
+            num1 = int(input("Enter first number:"))
+            num2 = int(input("Enter second number:"))
         except ValueError:
             print("Invalid input.Please enter a number.")
             continue
         if choice == '1':
-            print(num1,"+",num2,"=",add(num1,num2))
+            print(num1, "+", num2, "=", add(num1, num2))
         if choice == '2':
-            print(num1,"-",num2,"=",sub(num1,num2))
+            print(num1, "-", num2, "=", sub(num1, num2))
         if choice == '3':
-            print(num1,"*",num2,"=",multiply(num1,num2))
+            print(num1, "*", num2, "=", multiply(num1, num2))
         if choice == '4':
-            print(num1,"/",num2,"=",divide(num1,num2))
+            print(num1, "/", num2, "=", divide(num1, num2))
     if choice == '5':
         try:
             num = int(input("Enter a number:"))
         except ValueError:
             print("Invalid input.Please enter a number.")
             continue
-        print(num," square root =",sqr(num))
-    if choice== '6':
+        print(num, " square root =", sqr(num))
+    if choice == '6':
         try:
             num = int(input("Enter base number:"))
-            exp=int(input("enter exponentiation"))
+            exp = int(input("enter exponentiation"))
         except ValueError:
             print("Invalid input.Please enter a number.")
             continue
-        print(num,"^",exp,"=",power(num,exp))
-    next=input("let's do next calculation?(yes/no):")
+        print(num, "^", exp, "=", power(num, exp))
+    next = input("let's do next calculation?(yes/no):")
     if next == "no":
         break
     else:

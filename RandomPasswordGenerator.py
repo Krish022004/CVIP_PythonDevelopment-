d13b22108f2679f185346f47ca6a36f30fcfe0d0
gkt02 <gkt02feb2004@gmail.com>
2023-08-31 00:29:50 +0530

    These Python Development intern phase 1 golden project

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -1,15 +1,18 @@
 import random
 import string
 
+
 def generate(length):
-    characters= string.ascii_letters + string.digits + string.punctuation
+    characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.choice(characters) for i in range(length))
     return password
+
+
 while True:
-    length=int(input("Enter the length of the passwords:"))
-    password=generate(length)
-    print("Your random password is:",password)
-    next=input("let's do next calculation?(yes/no):")
+    length = int(input("Enter the length of the passwords:"))
+    password = generate(length)
+    print("Your random password is:", password)
+    next = input("let's do next calculation?(yes/no):")
     if next == "no":
         break
     else:
